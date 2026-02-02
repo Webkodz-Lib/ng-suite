d13cd06c1f8ced23cfa4f1155bc93ec0873e5524
varunsingh1991 <varunsingh91@Live.com>
2026-02-02 19:24:57 +0530

    removed action flow

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,9 +2,11 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    {provide: LocationStrategy, useClass: HashLocationStrategy},
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes)
   ]
